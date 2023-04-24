314b5105f465dfa50a0efd418611aae9e8540963
Sebastián Burgos <snburgos@uc.cl>
2023-04-24 00:03:19 -0400

    Changed variable names

--- lib/main.c
+++ lib/main.c
@@ -5,160 +5,160 @@
 // Estructura para representar el estado de la solución
 typedef struct
 {
-  int objeto;  // Índice del objeto
-  int mochila; // Índice de la mochila asignada
-} Estado;
+  int task;    // Índice del task
+  int cluster; // Índice de la cluster asignada
+} State;
 
 // Función para generar una solución inicial aleatoria
-void generarSolucionInicial(int n, int l, int pesos[], Estado asignaciones[])
+void generateInitialSolution(int n, int l, int times[], State assignments[])
 {
   for (int i = 0; i < n; i++)
   {
-    asignaciones[i].objeto = i;
-    asignaciones[i].mochila = rand() % l;
+    assignments[i].task = i;
+    assignments[i].cluster = rand() % l;
   }
 }
 
 // auxiliar
-// Función para imprimir la asignación de objetos a mochilas
-void imprimirAsignacion(int n, Estado asignaciones[])
+// Función para imprimir la asignación de tasks a clusters
+void printAssignment(int n, State assignments[])
 {
-  printf("Asignación de objetos a mochilas:\n");
+  printf("Asignación de tasks a clusters:\n");
 
   for (int i = 0; i < n; i++)
   {
-    printf("Objeto %d -> Mochila %d\n", i, asignaciones[i].mochila);
+    printf("Task %d -> Cluster %d\n", i, assignments[i].cluster);
   }
 }
 
-int *obtenerPesosCadaMochila(int l, Estado asignaciones[], int n, int pesos[])
+int *getTimesEachCluster(int l, State assignments[], int n, int times[])
 {
-  int *peso_mochilas = (int *)calloc(l, sizeof(int));
+  int *timesClusters = (int *)calloc(l, sizeof(int));
   for (int i = 0; i < n; i++)
   {
-    peso_mochilas[asignaciones[i].mochila] += pesos[i];
+    timesClusters[assignments[i].cluster] += times[i];
   }
-  return peso_mochilas;
+  return timesClusters;
 }
-void imprimirPesosCadaMochila(int l, Estado asignaciones[], int n, int pesos[])
+void printTimesEachCluster(int l, State assignments[], int n, int times[])
 {
-  printf("peso total de cada mochila:\n");
-  int *pesos_mochilas = obtenerPesosCadaMochila(l, asignaciones, n, pesos);
+  printf("Tiempo total de cada cluster:\n");
+  int *timesClusters = getTimesEachCluster(l, assignments, n, times);
   for (int i = 0; i < l; i++)
   {
-    printf("Mochila %d -> Peso %d\n", i, pesos_mochilas[i]);
+    printf("Cluster %d -> Tiempo %d\n", i, timesClusters[i]);
   }
 }
 
-int obtenerPesoDeMochilaMasPesada(int l, Estado asignaciones[], int n, int pesos[])
+int getMaxTimesFromCluster(int l, State assignments[], int n, int times[])
 {
-  int *pesos_mochilas = obtenerPesosCadaMochila(l, asignaciones, n, pesos);
-  int peso_maximo = 0;
+  int *timesClusters = getTimesEachCluster(l, assignments, n, times);
+  int maxTimes = 0;
   for (int i = 0; i < l; i++)
   {
-    if (pesos_mochilas[i] > peso_maximo)
+    if (timesClusters[i] > maxTimes)
     {
-      peso_maximo = pesos_mochilas[i];
+      maxTimes = timesClusters[i];
     }
   }
-  return peso_maximo;
+  return maxTimes;
 }
 
-void imprimirPesoTeorico(int n, int l, int pesos[])
+void printTheoricTimes(int n, int l, int times[])
 {
-  int peso_total = 0;
+  int totalTime = 0;
   for (int i = 0; i < n; i++)
   {
-    peso_total += pesos[i];
+    totalTime += times[i];
   }
-  int peso_teorico = peso_total / l;
-  // printf("Peso Total de los %d objetos-> %d\n", n, peso_total);
-  printf("Peso Teorico-> %d\n", peso_teorico);
+  int theoricTime = totalTime / l;
+  // printf("Tiempo Total de los %d tasks-> %d\n", n, totalTime);
+  printf("Tiempo teórico-> %d\n", theoricTime);
 }
 
-// Función para encontrar una solución vecina mediante la heurística de intercambio de un objeto
-Estado generarVecinoIntercambioObjeto(int indice_objeto, int l, int pesos[], Estado asignaciones[])
+// Función para encontrar una solución vecina mediante la heurística de intercambio de un task
+State generarVecinoIntercambioTask(int indexTask, int l, int times[], State assignments[])
 {
-  Estado vecino;
-  vecino.objeto = indice_objeto; // Seleccionar un objeto al azar
-  vecino.mochila = rand() % l;   // Seleccionar una mochila al azar
-  return vecino;
+  State neighbour;
+  neighbour.task = indexTask;     // Seleccionar un task al azar
+  neighbour.cluster = rand() % l; // Seleccionar una cluster al azar
+  return neighbour;
 }
 
-// calcula la mochila mas pesada de una asignacion dado un cierto vecino
-// luego retorna las asignaciones a su estado original
-int calcularPesoMaximoAsignacionTemporal(int l, Estado asignaciones[], int n, int pesos[], Estado vecino)
+// calcula la cluster mas pesada de una asignacion dado un cierto neighbour
+// luego retorna las assignments a su estado original
+int calculateTimeMaxTemporalAssignment(int l, State assignments[], int n, int times[], State neighbour)
 {
-  Estado asignacion_antigua = asignaciones[vecino.objeto];
-  asignaciones[vecino.objeto].mochila = vecino.mochila;
-  int pesoMochilaMasPesada = obtenerPesoDeMochilaMasPesada(l, asignaciones, n, pesos);
-  asignaciones[vecino.objeto].mochila = asignacion_antigua.mochila;
-  return pesoMochilaMasPesada;
+  State oldAssignation = assignments[neighbour.task];
+  assignments[neighbour.task].cluster = neighbour.cluster;
+  int timeMaxCluster = getMaxTimesFromCluster(l, assignments, n, times);
+  assignments[neighbour.task].cluster = oldAssignation.cluster;
+  return timeMaxCluster;
 }
 
-// Función para encontrar la solución vecina con menor peso de mochila
-Estado encontrarMejorVecino(int n, int l, int pesos[], Estado asignaciones[], Estado (*generarVecino)(int, int, int[], Estado[]))
+// Función para encontrar la solución vecina con menor tiempo de cluster
+State findBestNeighbour(int n, int l, int times[], State assignments[], State (*generarVecino)(int, int, int[], State[]))
 {
-  Estado mejorVecino = asignaciones[0];
-  int mejorPeso = obtenerPesoDeMochilaMasPesada(l, asignaciones, n, pesos);
+  State bestNeighbour = assignments[0];
+  int bestTime = getMaxTimesFromCluster(l, assignments, n, times);
 
   for (int i = 0; i < n; i++)
   {
-    // asignacion "item - mochila" aleatoria
-    Estado vecino = generarVecino(i, l, pesos, asignaciones);
-    // calculo el peso de esa mochila
-    int pesoVecino = calcularPesoMaximoAsignacionTemporal(l, asignaciones, n, pesos, vecino);
-    if (pesoVecino < mejorPeso)
+    // asignacion "item - cluster" aleatoria
+    State neighbour = generarVecino(i, l, times, assignments);
+    // calculo el tiempo de esa cluster
+    int tiempoVecino = calculateTimeMaxTemporalAssignment(l, assignments, n, times, neighbour);
+    if (tiempoVecino < bestTime)
     {
-      mejorVecino = vecino;
-      mejorPeso = pesoVecino;
+      bestNeighbour = neighbour;
+      bestTime = tiempoVecino;
     }
   }
-  return mejorVecino;
+  return bestNeighbour;
 }
 
 // Función para el algoritmo de Búsqueda Local
-void busquedaLocal(int n, int l, int pesos[], Estado asignaciones[], Estado (*generarVecino)(int, int, int[], Estado[]))
+void localSearch(int n, int l, int times[], State assignments[], State (*generarVecino)(int, int, int[], State[]))
 {
-  int iteraciones = 0;
-  while (iteraciones < 10000)
+  int iterations = 0;
+  while (iterations < 10000)
   {
-    Estado mejorVecino = encontrarMejorVecino(n, l, pesos, asignaciones, generarVecino);
-    asignaciones[mejorVecino.objeto].mochila = mejorVecino.mochila;
-    iteraciones++;
+    State bestNeighbour = findBestNeighbour(n, l, times, assignments, generarVecino);
+    assignments[bestNeighbour.task].cluster = bestNeighbour.cluster;
+    iterations++;
   }
 }
 
-char *solution(int n, int pesos[], int l)
+char *solution(int n, int times[], int l)
 {
   srand(time(NULL)); // Inicializar generador de números aleatorios
 
   // Generar una solución inicial aleatoria
-  Estado asignaciones[n];
-  generarSolucionInicial(n, l, pesos, asignaciones);
+  State assignments[n];
+  generateInitialSolution(n, l, times, assignments);
 
   // Imprimir la asignación inicial
-  // imprimirAsignacion(n, asignaciones);
-  imprimirPesosCadaMochila(l, asignaciones, n, pesos);
-  printf("mochila mas pesada -> %d\n", obtenerPesoDeMochilaMasPesada(l, asignaciones, n, pesos));
+  // printAssignment(n, assignments);
+  printTimesEachCluster(l, assignments, n, times);
+  printf("Cluster con más tiempo -> %d\n", getMaxTimesFromCluster(l, assignments, n, times));
 
   // Ejecutar el algoritmo de Búsqueda Local
-  busquedaLocal(n, l, pesos, asignaciones, generarVecinoIntercambioObjeto);
-  // busquedaLocal(n, l, pesos, asignaciones, generarVecinoIntercambioDosObjetos);
+  localSearch(n, l, times, assignments, generarVecinoIntercambioTask);
+  // localSearch(n, l, times, assignments, generarVecinoIntercambioDosTasks);
 
-  // Imprimir los pesos de cada mochila
-  imprimirPesosCadaMochila(l, asignaciones, n, pesos);
-  imprimirPesoTeorico(n, l, pesos);
-  imprimirAsignacion(n, asignaciones);
-  printf("mochila mas pesada -> %d\n", obtenerPesoDeMochilaMasPesada(l, asignaciones, n, pesos));
+  // Imprimir los times de cada cluster
+  printTimesEachCluster(l, assignments, n, times);
+  printTheoricTimes(n, l, times);
+  printAssignment(n, assignments);
+  printf("Cluster con más tiempo -> %d\n", getMaxTimesFromCluster(l, assignments, n, times));
 
-  // return a string with the asignaciones variable
-  char *asignaciones_string = (char *)malloc(n * sizeof(char));
+  // return a string with the assignments variable
+  char *assignmentsString = (char *)malloc(n * sizeof(char));
   for (int i = 0; i < n; i++)
   {
-    asignaciones_string[i] = asignaciones[i].mochila + '0';
+    assignmentsString[i] = assignments[i].cluster + '0';
   }
-  return asignaciones_string;
+  return assignmentsString;
 }
 
 int main()
